1c76ff5c12b12e24919f57ff87db0e761db7a33b
Enmanuel Velasquez Romero <enmanuel.velasquez@utp.edu.co>
2025-09-25 11:57:15 -0500

    fix: use params in parkVehicle; change == to equals; add README

--- src/main/java/com/example/Vehicle.java
+++ src/main/java/com/example/Vehicle.java
@@ -3,11 +3,11 @@ package com.example;
 public class Vehicle {
     String type;
     String registration;
-    String Color;
+    String color;
 
     public  Vehicle(String type, String registration, String color){
         this.type = type;
         this.registration = registration;
-        this.Color = color;
+        this.color = color;
     }
 }
