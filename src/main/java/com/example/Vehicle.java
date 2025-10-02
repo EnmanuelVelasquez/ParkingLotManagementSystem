e5c7d4c40af3e5e023c491a0648fe3026c6c6030
Enmanuel Velasquez Romero <enmanuel.velasquez@utp.edu.co>
2025-10-02 15:00:17 -0500

    change vehicle class and add abstraction
    I applied the abstraction cconcept to this class and implement encapsulationts concepts for the attributes and methods. I also did some validation adjustments for the attributes and created the setter and getters needed. I erased the type attribute so I'll need to implement it as enumarators in the following change

--- src/main/java/com/example/Vehicle.java
+++ src/main/java/com/example/Vehicle.java
@@ -1,13 +1,30 @@
 package com.example;
 
-public class Vehicle {
-    String type;
-    String registration;
-    String color;
+//We create vehicle as abstract because we're gonna use this as the 
+public abstract class Vehicle {
+    private final String licensePlate;
+    private final String color;
+    private String ownerName;
 
-    public  Vehicle(String type, String registration, String color){
-        this.type = type;
-        this.registration = registration;
-        this.color = color;
+
+    protected Vehicle(String licensePlate, String color, String ownerName){
+        //We validate to not have null information
+        if (licensePlate == null || licensePlate.isBlank()){
+            throw new IllegalArgumentException("Plate required");
+        }
+        // .trim helps us to avoid unwanted spaces and .toUpperCase helps us to use the capitalized word
+        this.licensePlate = licensePlate.trim().toUpperCase();
+        this.ownerName = ownerName;
+        //we use the 
+        this.color = color == null ? "unknown" : color.trim();
     }
+
+    //Do the getters of each attribute and the setter for the ownerName (simplified version)
+    public String getLicensePlate(){ return licensePlate; }
+    public String getColor(){ return color; }
+    public String getOwnerName() { return ownerName; }
+    public void setOwnerName(String ownerName) { this.ownerName = ownerName; }
+
+    //We create and abstract method that will help us to calculate the fee of every vehicle
+    public abstract double calculateFee (long minutesParked);
 }
