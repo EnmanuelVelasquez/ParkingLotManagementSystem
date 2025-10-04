bc1295f59c2acdf399cdd0dbc3bcc82a2eb50a8a
Enmanuel Velasquez Romero <enmanuel.velasquez@utp.edu.co>
2025-10-04 15:54:46 -0500

    Class slot updated and some methods added
    I updated and created attributes and methos from the slot class and I also added explanation comments.

--- src/main/java/com/example/Slot.java
+++ src/main/java/com/example/Slot.java
@@ -1,13 +1,27 @@
 package com.example;
 
 public class Slot {
-    String type;
+    private final SlotSize size;
     Vehicle vehicle;
     String ticketId;
 
-    public Slot(String type){
-        this.type = type;
+    public Slot(SlotSize size){
+        this.size = size;
         this.vehicle = null;
         this.ticketId = null;
+    }
+
+    //Getter of size
+    public SlotSize getSize(){ return size; }
+
+    //This method will help us to validate if the slot is available
+    public boolean isFree() {return vehicle == null; }
+
+    //This produces a quick description of the slot (useful for loggin or debugging)
+    //We also use override because we're changing the behaviour of the base object.toString method
+    @Override
+    public String toString(){
+        return "Slot{" + "size=" + size + ", ticketId=" + ticketId +
+               ", occupied=" + (vehicle != null) + '}';
     }
 }
