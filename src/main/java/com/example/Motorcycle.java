9ec6f94d2d55a0fc1ef16eab7b5375c6ddada744
Enmanuel Velasquez Romero <enmanuel.velasquez@utp.edu.co>
2025-10-04 15:36:10 -0500

    Validation to calculateFee and comments
    Validation to negative numbers and explanation comments added to the program.

--- src/main/java/com/example/Motorcycle.java
+++ src/main/java/com/example/Motorcycle.java
@@ -7,7 +7,10 @@ public class Motorcycle extends Vehicle{
 
     @Override
     public double calculateFee(long minutesParked){
+        //We use a simple validation with IllegalArgumentException to avoid receiving numbers less than 0
+        if (minutesParked < 0) throw new IllegalArgumentException ("minutesParked must be >= 0");
         double hourly = 1.0;
+        //We use math.ceil to round up a fractional number to the following whole number e.g 0.75 = 1.0 
         return Math.ceil(minutesParked / 60.0) * hourly;
     }
 
